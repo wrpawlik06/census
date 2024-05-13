b775ca34a6b8b0bfda962b78fa518f5fafc20978
wrpawlik06 <woj.rom.paw@gmail.com>
2024-05-13 19:54:50 +0100

    Added first version of builder.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,9 @@
 import streamlit as st
 import plotter
+import builder
 
+# Example usage within a Streamlit app
+if st.button("Download Files"):
+    builder.download_all_links()
+    
 plotter.plot_map()
