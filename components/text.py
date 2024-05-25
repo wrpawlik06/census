13c0dfcb2fba4e217617b67ce212d6ed4d2f4cfd
wrpawlik06 <woj.rom.paw@gmail.com>
2024-05-25 11:28:29 +0100

    Realizing that double quotes inside double quotes break strings lol

--- components/text.py
+++ components/text.py
@@ -5,9 +5,8 @@ def title_header():
     st.subheader("Use the sliders below to find your market :sunglasses:")
 
 def criteria_matches(df, df_filtered, matches):
-
-    st.write(f"Your 🏡Median Value criteria match {matches["house_price_matches"]} ({int(matches["house_price_matches"]/len(df)*100)}%) of zones.")
-    st.write(f"Your children under 16 years old % criteria match {matches["children_16_prct_matches"]} ({int(matches["children_16_prct_matches"]/len(df)*100)}%) of zones.")
-    st.write(f"Your children under 4 years old % criteria match {matches["children_4_prct_matches"]} ({int(matches["children_4_prct_matches"]/len(df)*100)}%) of zones.")
-    st.write(f"Your population density criteria match {matches["pop_den_matches"]} ({int(matches["pop_den_matches"]/len(df)*100)}%) of zones.")
+    st.write(f"Your 🏡Median Value criteria match {matches['house_price_matches']} ({int(matches['house_price_matches']/len(df)*100)}%) of zones.")
+    st.write(f"Your children under 16 years old % criteria match {matches['children_16_prct_matches']} ({int(matches['children_16_prct_matches']/len(df)*100)}%) of zones.")
+    st.write(f"Your children under 4 years old % criteria match {matches['children_4_prct_matches']} ({int(matches['children_4_prct_matches']/len(df)*100)}%) of zones.")
+    st.write(f"Your population density criteria match {matches['pop_den_matches']} ({int(matches['pop_den_matches']/len(df)*100)}%) of zones.")
     st.write(f"Your combined criteria match {len(df_filtered)} ({int(len(df_filtered)/len(df)*100)}%) of zones.")
